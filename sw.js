55e1a931454e76cf545db7329f8c5f5b3950a6af
jor134 <jor134@gmail.com>
2026-08-30 09:47:30 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE = "powderline-v1";
+const CORE = [
+  "./", "./index.html", "./manifest.webmanifest", "./icon.svg",
+  "https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js"
+];
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c =>
+    Promise.all(CORE.map(u => c.add(new Request(u, { mode: u.startsWith("http") ? "no-cors" : "same-origin" })).catch(() => {})))
+  ).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", e => {
+  e.waitUntil(caches.keys().then(ks =>
+    Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", e => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => hit || fetch(e.request).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+      return res;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
